56d945d3bc00d603931c6369bae406e7af25339c
Ben Surkiss <bensu@il.ibm.com>
2017-05-27 19:03:22 +0300

    Add new roles

---
+++ roles/PandaCounter-App/files/panda-counter-app/panda-counter.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var config = require('./config.json');
+var HttpDispatcher = require('httpdispatcher');
+var dispatcher = new HttpDispatcher();
+var counter = 0
+
+function handleRequest(request, response){
+    try {
+        console.log("Requested URL: " + request.url);
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+dispatcher.onGet("/", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('PandaCounter is at: ' + counter);
+});
+
+dispatcher.onPost("/", function(req, res) {
+        counter++;
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('You post is counted! Thank you dear panda!\n');
+    });
+
+dispatcher.onError(function(req, res) {
+        res.writeHead(404);
+        res.end("404 - Page Does not exists");
+});
+
+http.createServer(handleRequest).listen(config.port, function(){
+    console.log("Server listening on: http://localhost:%s", config.port);
+});
